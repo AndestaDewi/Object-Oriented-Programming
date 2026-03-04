6e232dfc9413e5bb2a18d67bae6b5911621be06f
AndestaDewi <andestadewi05@gmail.com>
2026-03-04 15:09:22 +0700

    Initial commit: Pertemuan 1

---
+++ Pertemuan-01/HelloOOP.java
@@ -0,0 +1,7 @@
+public class HelloOOP {
+    public static void main(String[] args) {
+        System.out.println("Hello OOP");
+        System.out.println("Selamat datang di Mata Kuliah PBO");
+        System.out.println("UNSIQ Wonsosobo");
+    }
+}
