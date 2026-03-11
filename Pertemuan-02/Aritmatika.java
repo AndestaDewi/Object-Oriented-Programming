fd1b891b2db7bb650350a89ab29199e50391fd55
AndestaDewi <andestadewi05@gmail.com>
2026-03-12 05:27:43 +0700

    Pertemuan 2

---
+++ Pertemuan-02/Aritmatika.java
@@ -0,0 +1,25 @@
+public class Aritmatika {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int c = 11;
+        int d = 4;
+
+        int penjumlahan = a + b;
+        int pengurangan = a - b;
+        int perkalian = a * b;
+        int pembagian = a / b;
+        int sisaBagi = c % d;
+
+        System.out.println("Nilai: a: " + a);
+        System.out.println("Nilai: b: " + b);
+        System.out.println("Nilai: c: " + c);
+        System.out.println("Nilai: d: " + d);
+
+        System.out.println("Penjumlahan: " + penjumlahan);
+        System.out.println("Pengurangan: " + pengurangan);
+        System.out.println("Perkalian: " + perkalian);
+        System.out.println("Pembagian: " + pembagian);
+        System.out.println("Sisa Bagi: " + sisaBagi);
+    }
+}
---
+++ Pertemuan-02/Biodata.java
@@ -0,0 +1,17 @@
+public class Biodata {
+    public static void main(String[] args) {
+        String nama = "Andesta Dewi";
+        String nim = "2024150080";
+        String jurusan = "Teknik Informatika";
+        int umur = 20;
+        String hobi = "music";
+        boolean mahasiswaAktif = true;
+
+        System.out.println("Nama : " + nama);
+        System.out.println("NIM : " + nim);
+        System.out.println("Jurusan : " + jurusan);
+        System.out.println("Umur : " + umur);
+        System.out.println("Hobi : " + hobi);
+        System.out.println("Mahasiswa Aktif : " + mahasiswaAktif);
+    }
+}
